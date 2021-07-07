d3dd03325649275d15702a2afc7b2c677a2ca1f3
Fahmi Akbar Wildana <sensorfied@gmail.com>
2021-07-08 04:08:46 +0700

    Enable cannon-es-debugger when in dev mode

--- src/viewport/index.tsx
+++ src/viewport/index.tsx
@@ -1,4 +1,4 @@
-import { Physics } from "@react-three/cannon";
+import { Debug, Physics } from "@react-three/cannon";
 import { GizmoHelper, GizmoViewport, OrbitControls, Preload, useAspect, useHelper } from "@react-three/drei";
 import { Suspense } from "react";
 import { BoxHelper, CameraHelper, Color, MathUtils, Vector2, Vector3 } from "three";
@@ -8,12 +8,15 @@ import { _3D } from "../component";
 import * as Environment from "./environment";
 
 export function Game() {
+    const Scene = <Suspense fallback={null}>
+        <_3D.Ball />
+        <_3D.Paddle />
+        <_3D.FallGround />
+    </Suspense>;
     return <>
         <Environment.Sunset />
         <Physics>
-            <_3D.Ball />
-            <_3D.Paddle />
-            <_3D.FallGround />
+            {import.meta.env.DEV ? <Debug>{Scene}</Debug> : Scene}
         </Physics>
         <OrbitControls />
     </>;
